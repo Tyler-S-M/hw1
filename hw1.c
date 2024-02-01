aa356f23df15c8b9b3f4c22821f54041b19bde23
Tyler-S-M <146662714+Tyler-S-M@users.noreply.github.com>
2024-01-31 18:02:37 -0600

    make blas timing accurate

--- hw1.c
+++ hw1.c
@@ -8,10 +8,9 @@
 #include <omp.h>
 #include <stdlib.h>
 #include <time.h>
-#include <cblas.h>
+#include "cblas.h"
 
-
-#define N 4196		// length of vector, length and width of matrix
+#define N 8192		// length of vector, length and width of matrix
 #define SEED 1		// random number seed
 #define REPS 1000	// repetitions
 //#define OUT   	// output flag
@@ -25,6 +24,11 @@ void vm_multiply_parallel();
 void vm_multiply_sequential();
 void output();
 
+void reset_vect(){
+	for (int i = 0; i < N; i ++)
+		result[i] = 0.0;
+}
+
 int main() {
 	int i;
 	time_t start;
@@ -37,12 +41,20 @@ int main() {
 	}
 	printf("Time Taken OpenMP: %ld\n", time(NULL) - start);
 
+	output();
 
-	start = time(NULL);
+	long aggregate = 0.0;
 	for (i=0; i<REPS; i++) {
+		start = time(NULL);
 		cblas_dgemv(CblasRowMajor, CblasNoTrans, N, N, 1.0, (const double*)matrix, N, vector, 1, 1.0, result, 1);
+		aggregate += (time(NULL) - start);
+
+		if (i != REPS-1)
+			reset_vect();
 	}
-	printf("Time Taken CBlas: %ld\n", time(NULL) - start);
+	printf("Time Taken CBlas: %ld\n", aggregate);
+
+	output();
 
 	start = time(NULL);
 	for (i=0; i<REPS; i++) {
@@ -96,27 +108,8 @@ void vm_multiply_sequential() {
 
 void output() {
 #ifdef OUT
-	int i,j;
-
-	printf("vector:");
-	for (i=0; i<N; i++) {
-		if (i%10 == 0)
-			printf("\n");
-		printf(" %6.4f",vector[i]);
-	}
-	printf("\n");
-	printf("matrix:\n");
-	for (i=0; i<N; i++) {
-		printf("row %d:",i);
-		for (j=0; j<N; j++) {
-			if (j%10 == 0)
-				printf("\n");
-			printf(" %6.4f",matrix[i][j]);
-		}
-		printf("\n");
-	}
 	printf("result:");
-	for (i=0; i<N; i++) {
+	for (int i=0; i<N; i++) {
 		if (i%10 == 0)
 			printf("\n");
 		printf(" %6.4f",result[i]);
